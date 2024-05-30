1fa57d5a637aa973d9255d9bd40455a2aec6655d
Quân Hoàng <quanhoang@Mories-Macbook.local>
2024-05-30 19:36:43 +0700

    fix socket

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 	useEffect(() => {
 		if (authUser) {
 			// const socket = io("https://chat-app-yt.onrender.com", {
-			const socket = io("http://localhost:4600", {
+			const socket = io("https://webchat-backend-bl39.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
